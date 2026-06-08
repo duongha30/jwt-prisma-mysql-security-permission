03c3eb988ba174f2e17cdb9437e6c4f6e8878bf1
duong.ha <duong.ha@viaccess-orca.com>
2026-06-09 00:34:37 +0300

    fix prisma service and eslint

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,12 +1,16 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../generated/prisma/client.js';
-import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL as string,
+    const adapter = new PrismaMariaDb({
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      connectionLimit: 5,
     });
     super({ adapter });
   }
